aab2f321f4e9814045218ad2c9485e69fa3b4564
Dan Lee <dlee1128@gmail.com>
2018-04-08 23:16:37 -0500

    Finished Product class

--- bird-store/src/com/jackssparrowaviary/store/Product.java
+++ bird-store/src/com/jackssparrowaviary/store/Product.java
@@ -1,9 +1,61 @@
 package com.jackssparrowaviary.store;
 
 public class Product {
-
-	public Product() {
-		// TODO Auto-generated constructor stub
+	private int id;
+	private String productName;
+	private double price;
+	private int quantity;
+	
+	public Product(String productName, double price, int quantity) {
+		this.productName = productName;
+		this.price = price;
+		this.quantity = quantity;
+	}
+	
+	public Product(int id, String productName, double price, int quantity) {
+		this.id = id;
+		this.productName = productName;
+		this.price = price;
+		this.quantity = quantity;
 	}
 
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	@Override
+	public String toString() {
+		return "Product [id=" + id + ", productName=" + productName + ", price=" + price + ", quantity=" + quantity
+				+ "]";
+	}
+
+	
 }
